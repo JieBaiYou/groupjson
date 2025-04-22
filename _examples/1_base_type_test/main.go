a92e1f539c4b407480ad2734132f0727563f5d48
JieBaiYou <jiebaiyou@example.com>
2025-04-23 00:10:38 +0800

    更新: 在示例中添加标准库和自定义Marshal方法输出JSON，用于对比输出

--- _examples/1_base_type_test/main.go
+++ _examples/1_base_type_test/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/JieBaiYou/groupjson"
@@ -25,7 +26,7 @@ func main() {
 	publicJSON, _ := groupjson.New().
 		WithGroups("public").
 		Marshal(user)
-	fmt.Println(string(publicJSON))
+	fmt.Printf("publicJSON: %s\n\n", string(publicJSON))
 	// 输出: {"id":1,"name":"张三"}
 
 	// 带选项的序列化
@@ -33,11 +34,32 @@ func main() {
 		WithGroups("admin", "internal").
 		WithTopLevelKey("data").
 		Marshal(user)
-	fmt.Println(string(adminJSON))
+	fmt.Printf("adminJSON: %s\n\n", string(adminJSON))
 	// 输出: {"data":{"email":"zhangsan@example.com","id":1,"name":"张三","password":"secret123"}}
 
 	// 使用Marshal解析
 	internalJSON, _ := groupjson.Marshal(user, "internal")
-	fmt.Println(string(internalJSON))
+	fmt.Printf("internalJSON: %s\n\n", string(internalJSON))
 	// 输出: {"password":"secret123"}
+
+	user = User{
+		ID:    1,
+		Name:  "张三",
+		Email: "zhangsan@example.com",
+		Password: `<div class="content" data-info='{"key": "value", "text": "He said, \"Hello <world> & welcome!\""}'>
+  <!-- 注释：这是一个 <测试> 区域 -->
+  <p title="提示 & 描述">这是一段包含 &lt;strong&gt;HTML&lt;/strong&gt; 的文本。</p>
+  <script>alert("你好，世界！ & \" < >");</script>
+</div>`,
+	}
+
+	// 使用标准库序列化
+	json, _ := json.Marshal(user)
+	fmt.Printf("json: %s\n\n", string(json))
+	// 输出: {"id":1,"name":"张三","email":"zhangsan@example.com","password":"\u003cdiv class=\"content\" data-info='{\"key\": \"value\", \"text\": \"He said, \\\"Hello \u003cworld\u003e \u0026 welcome!\\\"\"}'\u003e\n  \u003c!-- 注释：这是一个 \u003c测试\u003e 区域 --\u003e\n  \u003cp title=\"提示 \u0026 描述\"\u003e这是一段包含 \u0026lt;strong\u0026gt;HTML\u0026lt;/strong\u0026gt; 的文本。\u003c/p\u003e\n  \u003cscript\u003ealert(\"你好，世界！ \u0026 \\\" \u003c \u003e\");\u003c/script\u003e\n\u003c/div\u003e"}
+
+	// 使用Marshal解析
+	internalJSON, _ = groupjson.Marshal(user, "internal")
+	fmt.Printf("internalJSON: %s\n\n", string(internalJSON))
+	// 输出: {"password":"\u003cdiv class=\"content\" data-info='{\"key\": \"value\", \"text\": \"He said, \\\"Hello \u003cworld\u003e \u0026 welcome!\\\"\"}'\u003e\n  \u003c!-- 注释：这是一个 \u003c测试\u003e 区域 --\u003e\n  \u003cp title=\"提示 \u0026 描述\"\u003e这是一段包含 \u0026lt;strong\u0026gt;HTML\u0026lt;/strong\u0026gt; 的文本。\u003c/p\u003e\n  \u003cscript\u003ealert(\"你好，世界！ \u0026 \\\" \u003c \u003e\");\u003c/script\u003e\n\u003c/div\u003e"}
 }
